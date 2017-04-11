4318177d7ece6540c8c5dda59ac7ffedb3bd199b
Robert Littlejohn <bbblittlejohn@gmail.com>
2017-04-11 01:50:34 -0700

    modifies error handling strategy
    sends error message in repsonse instead of throwing an error for all helpers

--- server/helpers.js
+++ server/helpers.js
@@ -14,7 +14,7 @@ helpers.getExpenses= function(req, res) {
     let userName = req.params.user;
     m.query('SELECT * FROM expenses LEFT JOIN users ON expenses.owner_id = users.user_id where name = :userName;', {userName}, function(err, rows){
         if (err){
-            throw err;
+            res.send(err.message);
         }
         res.send(rows);
     });
@@ -24,7 +24,7 @@ helpers.getAnExpense = function(req, res) {
     let expenseId = req.params.expenseId;
     m.query('SELECT * FROM expenses where expense_id = :expenseId', {expenseId}, function(err, rows){
         if (err){
-            throw err;
+            res.send(err.message);
         }
         res.send(rows);
     });
@@ -35,7 +35,6 @@ helpers.createExpense = function(req, res) {
     m.query('INSERT INTO expenses (date_time, amount, description, owner_id) VALUES (:date_time, :amount, :description, :ownerId)', expense, function(err, info){
         if (err){
             res.send(err.message);
-            throw err;
         } else {
             res.send(info);
         }
@@ -48,7 +47,6 @@ helpers.updateExpense = function(req, res) {
     m.query('UPDATE expenses SET date_time = :dateTime, amount = :amount, description = :description, owner_id = :ownerId where expense_id = :expenseId', expense, function(err, info){
         if (err){
             res.send(err.message);
-            throw err;
         } else {
             res.send(info);
         }
@@ -60,7 +58,6 @@ helpers.deleteExpense = function(req, res) {
     m.query('DELETE FROM expenses where expense_id = :expenseId', {expenseId}, function(err, info){
         if (err){
             res.send(err.message);
-            throw err;
         } else {
             res.send(info);
         }
