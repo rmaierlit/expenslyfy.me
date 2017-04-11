954f4ebd02f46f062a872c92303789b90a5d5304
Robert Littlejohn <bbblittlejohn@gmail.com>
2017-04-10 22:30:58 -0700

    added test expense

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -14,11 +14,15 @@ INSERT INTO users (name) VALUES ('Ulysses');
 INSERT INTO users (name, is_admin) VALUES ('JUNO', TRUE);
 
 CREATE TABLE expenses (
-	id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	title VARCHAR(200) NOT NULL,
+	expense_id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    date_time DATE DEFAULT NULL,
+    amount DECIMAL(12,2) DEFAULT NULL,
+	description VARCHAR(200) DEFAULT NULL,
 	owner_id SMALLINT UNSIGNED NOT NULL,
 	CONSTRAINT `fk_expenses_users`
 		FOREIGN KEY (owner_id) REFERENCES users (user_id)
 		ON DELETE CASCADE
 		ON UPDATE RESTRICT
-)
+);
+
+INSERT INTO expenses (amount, description, owner_id) VALUES ('3600.02', 'Candles', 1);
