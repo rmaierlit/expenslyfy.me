0116c23aed3fcb4686b683ecde7d2458e08b6ff6
Robert Littlejohn <bbblittlejohn@gmail.com>
2017-04-11 14:08:51 -0700

    added test for GET API routes
    the individual expense get route currently fails the test because the database stores a null date
    a test schema for the database or some kind of stub should be added in the future

--- test/test.js
+++ test/test.js
@@ -10,6 +10,37 @@ describe ('GET /', function() {
     });
 });
 
-// describe ('GET /users/:user', function () {
+describe ('GET /users/:user/expenses', function () {
+    it('should respond with a JSON array of expenses', function(done){
+        request(server)
+            .get('/users/Wint/expenses')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200, done);
+    });
+});
 
-// });
+describe ('GET /users/:user/expenses', function() {
+    it('should respond with a JSON expense object with dateTime, amount, and description properties', function(done){
+
+        function hasProperties(res){
+            console.log(res.body);
+            if (!('date_time' in res.body)){
+                throw new Error('missing dateTime property');
+            }
+            if (!('amount' in res.body)){
+                throw new Error('missing amount property');
+            }
+            if (!('description' in res.body)){
+                throw new Error('missing description property');
+            }
+        }
+
+        request(server)
+            .get('/users/Wint/expenses/1')
+            .set('Accept', 'application')
+            .expect('Content-Type', /json/)
+            .expect(hasProperties)
+            .end(done);
+    });
+});
