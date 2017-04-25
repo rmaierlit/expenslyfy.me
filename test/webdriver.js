d626f17a7f815c87dcce480a5219f25c8d48df2d
Robert Littlejohn <bbblittlejohn@gmail.com>
2017-04-24 18:04:34 -0700

    adds web itegration tests for logging in and logging out using Selenium

--- test/webdriver.js
+++ test/webdriver.js
@@ -1,5 +1,6 @@
 /*eslint-env node, mocha*/
 var selenium = require("selenium-webdriver");
+var By = selenium.By;
 var expect = require("chai").expect;
 
 var driver = new selenium.Builder()
@@ -8,17 +9,40 @@ var driver = new selenium.Builder()
 
 before(function() {
     this.timeout(10000)
-    driver.getWindowHandle()
+    return driver.getWindowHandle()
 });
 
 after(function() {
-    driver.quit();
-})
+    return driver.quit();
+});
 
 describe('Integration Tests', function() {
     beforeEach(function(){
-        this.driver.get('http://localhost:6565')
-    })
-    driver.getTitle().then(title => console.log(title));
-    expect(true).to.equal(true);
+        return driver.get('localhost:6565');
+    });
+    it('should dislpay the title', function() {
+        return driver.findElement(By.css('h1')).getText()
+            .then(text => expect(text).to.equal("Expenslyfy.me"));
+    });
+    it('should begin at the login screen', function() {
+        return driver.findElement(By.css('#login h2')).getText()
+            .then(text => expect(text).to.equal("Login:"));
+    });
+    it('should allow the user to sign in with their username and password', function(){
+        return driver.findElement(By.css('[data-name="user"]')).sendKeys('Wint')
+            .then(_ => driver.findElement(By.css('[data-name="password"]')).sendKeys('@dril'))
+            .then(_ => driver.findElement(By.css('#login button')).click())
+            .then(_ => driver.findElement(By.css('#login h2')).getText())
+            .then(text => expect(text).to.equal("Logged In As Wint"))
+    });
+    it('should allow the user to log out', function() {
+        return driver.findElement(By.css('[data-name="user"]')).sendKeys('Wint')
+            .then(_ => driver.findElement(By.css('[data-name="password"]')).sendKeys('@dril'))
+            .then(_ => driver.findElement(By.css('#login button')).click()) //logging in
+            .then(_ => driver.findElement(By.css('#login h2')).getText())
+            .then(text => expect(text).to.equal("Logged In As Wint"))
+            .then(_ => driver.findElement(By.css('#login button')).click()) //logging out
+            .then(_ => driver.findElement(By.css('#login h2')).getText())
+            .then(text => expect(text).to.equal("Login:"));
+    });
 });
